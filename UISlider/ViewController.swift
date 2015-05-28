9a91831513f30248f1f51abceaeffe0fad8ea1c5
Yelena Barilo <ybarilo@lynda.com>
2015-05-28 00:19:25 -0700

    Updated Color Maker icons, added a boarder and added a reset button to the bottom nav.

--- UISlider/ViewController.swift
+++ UISlider/ViewController.swift
@@ -15,21 +15,38 @@ class ViewController: UIViewController {
     @IBOutlet weak var redControl: UISlider!
     @IBOutlet weak var greenControl: UISlider!
     @IBOutlet weak var blueControl: UISlider!
+    @IBOutlet weak var instructionalLabel: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-
+        
+        colorView.layer.cornerRadius = 5
+        colorView.layer.borderWidth = 1
+        
+        colorView.backgroundColor = UIColor.grayColor()
     }
+    
+    
     
     @IBAction func changeColorComponent(sender: AnyObject) {
         
+        instructionalLabel.hidden = true
         let r: CGFloat = CGFloat(self.redControl.value)
         let g: CGFloat = CGFloat(self.greenControl.value)
         let b: CGFloat = CGFloat(self.blueControl.value)
         
         colorView.backgroundColor = UIColor(red: r, green: g, blue: b, alpha: 1)
-        
+        //r.text = "\(value)"
     }
     
+    @IBAction func reserColorView(sender: AnyObject) {
+        
+        instructionalLabel.hidden = false
+        
+        colorView.backgroundColor = UIColor.grayColor()
+        redControl.value = 0.5
+        greenControl.value = 0.5
+        blueControl.value = 0.5
+    }
 }

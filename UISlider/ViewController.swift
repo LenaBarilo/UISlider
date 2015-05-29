b491bdf8fa393239b0da2deba2baed4aa0d3889f
Yelena Barilo <ybarilo@lynda.com>
2015-05-28 22:56:46 -0700

    Updated share button

--- UISlider/ViewController.swift
+++ UISlider/ViewController.swift
@@ -11,49 +11,44 @@ import UIKit
 class ViewController: UIViewController {
     
     @IBOutlet weak var colorView: UIView!
+    @IBOutlet weak var instructionalLabel: UILabel!
     
     @IBOutlet weak var redControl: UISlider!
     @IBOutlet weak var greenControl: UISlider!
     @IBOutlet weak var blueControl: UISlider!
-    @IBOutlet weak var instructionalLabel: UILabel!
-    
+
     @IBOutlet weak var rLabel: UILabel!
     @IBOutlet weak var gLabel: UILabel!
     @IBOutlet weak var bLabel: UILabel!
     
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        
         colorView.layer.cornerRadius = 5
         colorView.layer.borderWidth = 1
         colorView.backgroundColor = UIColor.grayColor()
     }
     
     @IBAction func changeColorComponent(sender: AnyObject) {
-        
         instructionalLabel.hidden = true
         var r: CGFloat = CGFloat(self.redControl.value)
         let g: CGFloat = CGFloat(self.greenControl.value)
         let b: CGFloat = CGFloat(self.blueControl.value)
         
         colorView.backgroundColor = UIColor(red: r, green: g, blue: b, alpha: 1)
-
         returnColorValue()
     }
     
     // Return UI Slider color value numerically
     func returnColorValue() {
-
         rLabel.text = redControl.value.description
         gLabel.text = greenControl.value.description
         bLabel.text = blueControl.value.description
     }
     
-    @IBAction func reserColorView(sender: AnyObject) {
-        
+    @IBAction func resetColorView(sender: AnyObject) {
         instructionalLabel.hidden = false
-        
         colorView.backgroundColor = UIColor.grayColor()
         
         redControl.value = 0.5
@@ -69,7 +64,7 @@ class ViewController: UIViewController {
     @IBAction func shareColor(sender: AnyObject) {
         
         let activityViewController = UIActivityViewController(
-            activityItems: ["This color is awesome! \nRed value is \(redControl.value.description), green value is \(greenControl.value.description) and blue value is \(blueControl.value.description))" as NSString],
+            activityItems: ["This color is awesome! \nRed value is \(redControl.value.description), \nGreen value is \(greenControl.value.description) \nBlue value is \(blueControl.value.description)" as NSString],
             applicationActivities: nil)
         
         presentViewController(activityViewController, animated: true, completion: nil)
